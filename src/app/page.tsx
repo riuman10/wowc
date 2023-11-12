4071b93cf5a73344270262b55729e4c710b1b09b
riumaan10 <riuman.21@icloud.com>
2023-11-12 12:51:03 +0500

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [open , setOpen] = useState(false);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={() => setOpen(!open)} full = {false} title = {open ? "Close" : "Open"} />
+      <Button onClick={() => setOpen(!open)} title = {open ? "Close" : "Open"} />
       {
         open ? (
           <div className='flex items-center'>
